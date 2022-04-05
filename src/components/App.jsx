ad3d85fb0294c8f53abc8b692af7d1f966cc16e2
IhorKozloff <ik.eyeshield21@gmail.com>
2022-04-05 23:13:49 +0300

    prepare FeedBack +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
+import { FeedBack } from 'components/FeedBack/Feed-Back'
+
+const feedback = new FeedBack;
+
+
 export const App = () => {
   return (
     <div>
-      
+      {feedback.render()}
     </div>
   );
 };
---
+++ src/components/FeedBack/Feed-Back.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Title } from 'components/FeedBack/Feed-Back.styled';
+
+export class FeedBack extends Component {
+
+
+
+
+    render() {
+        return (
+            <div>
+               <Title>Please leave feedback</Title>
+               <Title>Statistics</Title> 
+            </div>
+        )
+    }
+};
